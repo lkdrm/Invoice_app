ec8e4d4d895d6cbc357d4c6583d24456c93945d0
LkDrm <larik115@gmail.com>
2026-03-24 18:49:59 +0100

    Add first models
    Signed-off-by: LkDrm <larik115@gmail.com>

---
+++ Invoice Builder App/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Invoice_Builder_App.Models;
+
+public class Customer
+{
+    public Guid Id { get; set; }
+
+    [EmailAddress]
+    public string Email { get; set; }
+
+    public string PostalCode { get; set; }
+
+    public TaxInfo Tax { get; set; }
+
+    public string Address { get; set; }
+
+    public UserInfo User { get; set; }
+
+    public string BankDetails { get; set; }
+}
